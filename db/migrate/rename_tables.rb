2ea403ae6ce3ddf9bab3c8a2a7751180ee913ab3
Rob Peace <rob.peace@gmail.com>
2014-07-02 17:36:56 -0400

    This time, the migration issues might be fixed.

--- db/migrate/rename_tables.rb
+++
@@ -1,9 +0,0 @@
-class RenameTables < ActiveRecord::Migration
-  def self.up
-      rename_table :items :api_items
-      rename_table :heros :api_heros
-      rename_table :matches :api_matches
-      rename_table :series :api_series
-      rename_table :performances :api_performances
-  end
-end
