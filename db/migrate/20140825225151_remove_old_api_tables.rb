7dc32028340c848ff6d7a2e0b26e58e1e3263e2b
Rob Peace <rob.peace@gmail.com>
2014-09-09 18:38:45 -0400

    Still working on that old migration

--- db/migrate/20140825225151_remove_old_api_tables.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOldApiTables < ActiveRecord::Migration
-  def change
-  	drop_table :api_heros
-  	drop_table :api_items
-  	drop_table :api_matches
-  	drop_table :api_performances
-  	drop_table :api_series
-  end
-end
