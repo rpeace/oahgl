7184c8d92c08a299e494845213b45276872b718a
Rob Peace <rob.peace@gmail.com>
2014-09-09 19:49:07 -0400

    Trying to support bigints in Steam UID round 2

--- db/migrate/20140909234127_support_big_ints_in_steam_ui_ds.rb
+++ db/migrate/20140909234127_support_big_ints_in_steam_ui_ds.rb
@@ -1,5 +1,5 @@
 class SupportBigIntsInSteamUiDs < ActiveRecord::Migration
   def change
-  	change_column :users, :uid, :integer, :limit => 8
+  	change_column :users, :uid, 'integer USING CAST(uid AS integer)', :limit => 8
   end
 end
